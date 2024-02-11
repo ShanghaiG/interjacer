17bb5e76cd4218ccba52cdd0082cda1d26fe3914
Robert-Dudzinski <dudzinski.robert97@gmail.com>
2024-02-11 04:05:51 +0100

    Update CarouselLayerSelector.tsx

--- interjacer-app/src/components/Carousel/components/CarouselLayerSelector/CarouselLayerSelector.tsx
+++ interjacer-app/src/components/Carousel/components/CarouselLayerSelector/CarouselLayerSelector.tsx
@@ -14,13 +14,13 @@ export const CarouselLayerSelector = ({
   return (
     <div
       className="absolute md:bottom-5 bottom-2 h-[25px] left-1/2 translate-x-[-50%] z-50 select-none cursor-pointer"
-      style={{ width: `${numberOfLayers * 35}px` }}
+      style={{ width: `${numberOfLayers * 30}px` }}
     >
       {Array.from({ length: numberOfLayers }).map((_, index) => (
         <div
           key={index}
           className={twJoin(
-            "md:w-[20px] md:h-[20px] absolute rounded-full duration-150 flex justify-center items-center border-8",
+            "md:w-[20px] md:h-[20px] w-[10px] h-[10px] absolute rounded-full duration-150 flex justify-center items-center border-4 md:border-8",
             index === selectedLayer
               ? "bg-slate-800 border-slate-100"
               : "bg-slate-100 border-slate-800"
